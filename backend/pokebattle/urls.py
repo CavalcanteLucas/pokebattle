7b082524d3cd31ac3d847fa1223320a969420a2d
Lucas Cavalcante <thesupervisar@gmail.com>
2020-02-19 17:46:39 -0300

    removing division_by_zero test for sentry

--- backend/pokebattle/urls.py
+++ backend/pokebattle/urls.py
@@ -4,13 +4,8 @@ from django.views.generic import TemplateView
 
 import django_js_reverse.views
 
-from django.urls import path
-
-def trigger_error(request):
-    division_by_zero = 1 / 0
 
 urlpatterns = [
-    url(r"^sentry-debug/", trigger_error),
     url(r"^admin/", admin.site.urls),
     url(r"^jsreverse/$", django_js_reverse.views.urls_js, name="js_reverse"),
     url(r"^$", TemplateView.as_view(template_name="itworks.html"), name="home"),
