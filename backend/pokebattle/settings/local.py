8af05e25f18af9254b022343a859fa97aed42f6f
Lucas Cavalcante <thesupervisar@gmail.com>
2020-02-19 16:41:15 -0300

    testing sentry support

--- backend/pokebattle/settings/local.py
+++ backend/pokebattle/settings/local.py
@@ -1,1 +1,13 @@
 from .local_base import *  # noqa
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://3f7b02fa6ff14d3eb856a680e77695e6@sentry.io/2686701",
+    integrations=[DjangoIntegration()],
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
